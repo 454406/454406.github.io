3f919b07deff17d4465a695c3de7a2015079efcd
Theeuwen,Sander S.A.G <theeuwen.sander@gmail.com>
2022-12-29 21:31:03 +0100

    Java Spring boot test

---
+++ src/main/java/nl/sander/theeuwen/portfolio/controllers/TestController.java
@@ -0,0 +1,17 @@
+package nl.sander.theeuwen.portfolio.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController
+{
+    @GetMapping
+    public String test()
+    {
+        System.out.println("Test invoked");
+        return "Test succeeded";
+    }
+}
---
+++ src/test/java/nl/sander/theeuwen/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.sander.theeuwen.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
